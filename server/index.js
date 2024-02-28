463afdf9dbcaf5c86cd8aeac45ec7a7439f455b2
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-02-28 15:02:22 -0300

    adding a new book with pictures

--- server/index.js
+++ server/index.js
@@ -1,107 +1,160 @@
 require("dotenv").config();
 const cors = require("cors");
-const connectDB = require("./connectDB");
 const express = require("express");
-const Book = require("./models/Books");
+const connectDB = require("./connectDB");
+const Book = require('./models/Books');
+const multer = require("multer");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 connectDB();
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded( { extended: true } ));
 app.use(express.json());
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
 
-// get books
+// Get All Books
 app.get("/api/books", async (req, res) => {
   try {
-
     const category = req.query.category;
-    // const stars = req.query.stars;
-    // console.log(category);
+    //const stars = req.query.stars;
 
     const filter = {};
     if(category) {
-      filter.category = category
-
+      filter.category = category;
     }
 
-
-    // const data = await Book.find({}).limit(2);
     const data = await Book.find(filter);
-    res.json(data );
+    
+    if (!data) {
+      throw new Error("An error occurred while fetching books.");
+    }
+    
+    res.status(201).json(data);
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching books." });
   }
 });
 
-
-
-
-// book page
+// Get A Single Books
 app.get("/api/books/:slug", async (req, res) => {
   try {
+    const slugParam = req.params.slug;
+    const data = await Book.findOne({ slug: slugParam});
 
-    const slupParam = req.params.slug
-    const data = await Book.findOne({slug: slupParam});
-    res.json(data );
+    if (!data) {
+      throw new Error("An error occurred while fetching a book.");
+    }
+    
+    res.status(201).json(data);
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching books." });
   }
 });
 
 
-// add book
-app.post("/api/books", async (req, res) => {
+
+// Create A Book
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  }
+})
+
+const upload = multer({ storage: storage })
+
+app.post("/api/books", upload.single("thumbnail")  ,async (req, res) => {
   try {
-console.log(req.body);
-   
+    console.log(req.body);
+    console.log(req.file);
+
     const newBook = new Book({
       title: req.body.title,
       slug: req.body.slug,
       stars: req.body.stars,
       description: req.body.description,
       category: req.body.category,
-      // thumbnail: req.body.thumbnail
+      thumbnail: req.file.filename,
     })
-    await Book.create(newBook)
-    res.json("data submited" );
+
+    await Book.create(newBook);
+    res.json("Data Submitted");
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching books." });
+  }
+});
+
+// Update A Book
+app.put("/api/books", upload.single("thumbnail"), async (req, res) => {
+  try {
+
+    const bookId = req.body.bookId;
+
+    const updateBook = {
+      title: req.body.title,
+      slug: req.body.slug,
+      stars: req.body.stars,
+      description: req.body.description,
+      category: req.body.category,
+    }
+
+    if (req.file) {
+      updateBook.thumbnail = req.file.filename;
+    }
+
+    await Book.findByIdAndUpdate(bookId, updateBook)
+    res.json("Data Submitted");
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching books." });
   }
 });
 
 
+app.delete("/api/books/:id", async(req,res) => {
+  const bookId = req.params.id;
+
+  try {
+    await Book.deleteOne({_id: bookId});
+    res.json("How dare you!" + req.body.bookId);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 
+// app.post("/api/books", async (req, res) => {
+//   try {
+//     console.log(req.body);
 
+//     const newBook = new Book({
+//       title: req.body.title,
+//       slug: req.body.slug,
+//       stars: req.body.stars,
+//       description: req.body.description,
+//       category: req.body.category,
+//       //thumbnail: req.file.thumbnail,
+//     })
 
-
-
-
-
-
-
+//     await Book.create(newBook);
+//     res.json("Data Submitted");
+//   } catch (error) {
+//     res.status(500).json({ error: "An error occurred while fetching books." });
+//   }
+// });
 
 app.get("/", (req, res) => {
-  res.json("Hello Mate!");
+  res.json("Hello mate!");
 });
+
 app.get("*", (req, res) => {
   res.sendStatus("404");
 });
-app.get("/", (req, res) => {
-  res.json("Hello Mate!");
-});
-app.listen(PORT, () => {
+
+app.listen(PORT, ()=> {
   console.log(`Server is running on Port: ${PORT}`);
 });
-
-
-
-
-
-
-
-// 1:35:58 / 2:02:53 Add Book
-
